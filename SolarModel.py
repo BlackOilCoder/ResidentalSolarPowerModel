f8662d5d9a9d4acf88e9d3eb4bb8698e428b7c42
BlackOilCoder <mjdionne@gmail.com>
2022-09-18 14:45:26 -0500

    Updated to use a checkbox for the solar losses input

--- SolarModel.py
+++ SolarModel.py
@@ -54,12 +54,10 @@ with st.sidebar.expander("Solar System Description",expanded = True):
         return arrayTypeChoices[option]
     arrayType = st.selectbox('Array Type',options=list(arrayTypeChoices.keys()),format_func=format_func_array,index=1)
    
-    sysLossSelect = st.selectbox('System Loss Calc Method', ['Basic','Advanced'],index=0)
-    #sysLossSelect = st.checkbox('Advanced Loss Calc')
+    #sysLossSelect = st.selectbox('System Loss Calc Method', ['Basic','Advanced'],index=0)
+    sysLossSelect = st.checkbox('Advanced Loss Calc')
     
-    if sysLossSelect == "Basic":
-        systemLosses = st.number_input('System Losses (%)',min_value=(0.0),max_value=(100.0),value=11.42)
-    else:
+    if sysLossSelect:
         soiling = st.number_input('Soiling (%)',min_value=(0.0),max_value=(100.0),value=2.0)
         shading = st.number_input('Shading (%)',min_value=(0.0),max_value=(100.0),value=3.0)
         snow = st.number_input('Snow (%)',min_value=(0.0),max_value=(100.0),value=0.0)
@@ -71,7 +69,9 @@ with st.sidebar.expander("Solar System Description",expanded = True):
         age = st.number_input('Age (%)',min_value=(0.0),max_value=(100.0),value=0.0)
         availability = st.number_input('Availability (%)',min_value=(0.0),max_value=(100.0),value=0.0)
         systemLosses = (1-1*(1-soiling/100)*(1-shading/100)*(1-snow/100)*(1-mismatch/100)*(1-wiring/100)*(1-connections/100)*(1-initialDegradation/100)*(1-nameplateRating/100)*(1-age/100)*(1-availability/100))*100
-        st.number_input('System Losses (%)',min_value=(0.0),max_value=(100.0),value=systemLosses,disabled=False)
+        st.number_input('System Losses (%)',min_value=(0.0),max_value=(100.0),value=systemLosses,disabled=False)       
+    else:
+        systemLosses = st.number_input('System Losses (%)',min_value=(0.0),max_value=(100.0),value=11.42)
     
     tilt = st.number_input('Tilt (deg)',min_value=(0),max_value=(45),value=20)
     azimuth = st.number_input('Azimuth (deg)',min_value=(0),max_value=(360),value=180)
@@ -83,7 +83,7 @@ with st.sidebar.expander("Solar System Description",expanded = True):
         groundCovRatio = st.number_input('Ground Coverage Ratio',min_value=(0.1),max_value=(1.0),value=0.4)
     else:
         dcToACRatio = 1.2
-        inverterEff = 96
+        inverterEff = 96.0
         groundCovRatio = 0.4
     
 #Electric Conctract Setup
@@ -91,10 +91,10 @@ with st.sidebar.expander("Solar System Description",expanded = True):
 
 #NREL request building
 @st.cache
-def GetNRELData(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth):
+def GetNRELData(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth, dcToACRatio, inverterEff, groundCovRatio):
     myAPIkey = "9zzEI7dNgn4vk8706ibfhr9XPbzn7eKXIGc3TgMp"
     df = pd.DataFrame()
-    jsonRequestStr = "&address="+str(location)+"&system_capacity="+str(dcSysSize)+"&module_type="+str(moduleType)+"&array_type="+str(arrayType)+"&losses="+str(systemLosses)+"&tilt="+str(tilt)+"&azimuth="+str(azimuth)
+    jsonRequestStr = "&address="+str(location)+"&system_capacity="+str(dcSysSize)+"&module_type="+str(moduleType)+"&array_type="+str(arrayType)+"&losses="+str(systemLosses)+"&tilt="+str(tilt)+"&azimuth="+str(azimuth)+"&dc_ac_ratio="+str(dcToACRatio)+"&gcr="+str(groundCovRatio)+"&inv_eff="+str(inverterEff)
       
     request = "https://developer.nrel.gov/api/pvwatts/v6.json?api_key="+myAPIkey+jsonRequestStr+"&timeframe=hourly"
     response = requests.get(request)
@@ -102,16 +102,16 @@ def GetNRELData(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, 
     return df
 
 
-def RunCase(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth):
+def RunCase(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth, dcToACRatio, inverterEff, groundCovRatio):
     global dfActivecase
-    dfJson = GetNRELData(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth)
+    dfJson = GetNRELData(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth, dcToACRatio, inverterEff, groundCovRatio)
     dfActivecase['%SolarGen']=dfJson.iloc[0,dfJson.columns.get_loc("outputs.ac")]
     st.write(dfActivecase['%SolarGen'])
     return None
 
 #st.sidebar.button('Calculate Case',on_click=RunCase,args=(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth))
 if runcasebutton:
-    RunCase(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth)
+    RunCase(location, dcSysSize, moduleType, arrayType, systemLosses, tilt, azimuth, dcToACRatio, inverterEff, groundCovRatio)
     
     
 
